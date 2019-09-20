7845e2cd07bcd835145dd06b365987c4cba9631d
tk-ozawa <t.aroo1512@gmail.com>
2019-09-21 02:36:37 +0900

    showListメソッド追加

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,11 @@
 /**
  * ログイン
  */
-Route::get("/", "LoginController@goLogin");
-Route::post("/login", "LoginController@login");
+Route::get("/", "LoginController@goLogin");	// ログイン画面表示処理
+Route::post("/login", "LoginController@login");	// ログイン処理
+Route::get("/logout", "LoginController@logout");	// ログアウト処理
+
+/**
+ * レポート管理
+ */
+Route::get("/reports/showList", "ReportController@showList");	// レポート一覧表示処理
