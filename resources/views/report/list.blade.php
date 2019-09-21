f5528637029fb3341b6c55903727d23da9d6d1d3
tk-ozawa <t.aroo1512@gmail.com>
2019-09-22 00:16:54 +0900

    ファイル名reportList->list / 一覧表示機能

--- resources/views/report/list.blade.php
+++ resources/views/report/list.blade.php
@@ -5,12 +5,15 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<title>レポートリスト画面</title>
 </head>
 <body>
 	<nav class="navbar navbar-light bg-light">
-		<h1>レポート情報リスト</h1>
-		<a class="btn btn-primary" href="/sharereports/public/logout" role="button">ログアウト</a>
+		<a href="./showList"><h1>レポート管理システム</h1></a>
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item"><a class="btn btn-primary" href="./goAdd" role="button">新規作成</a></li>
+			<li class="nav-item"><a class="btn btn-danger" href="/sharereports/public/logout" role="button">ログアウト</a></li>
+		</ul>
 	</nav>
 	<div class="card-deck">
 		<div class="card">
