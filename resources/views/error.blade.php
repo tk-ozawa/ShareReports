bd193722f73345dff486b8f9a63d64af23c257b2
tk-ozawa <t.aroo1512@gmail.com>
2019-09-22 02:49:24 +0900

    サーバ処理エラーの場合のページ作成

---
+++ resources/views/error.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="author" content="Shinzo SAITO">
+		<title>Error | レポート管理システム</title>
+	</head>
+	<body>
+		<h1>Error</h1>
+		<section>
+			<h2>申し訳ございません。障害が発生しました。</h2>
+			<p>
+				以下のメッセージご確認ください。<br>
+				{{ $errorMsg }}
+			</p>
+		</section>
+		<p><a href="/sharereports/public/reports/showList">TOPへ戻る</a></p>
+	</body>
+</html>
