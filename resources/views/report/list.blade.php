6c06353fc642f4d1e8945f36acb335aca845ea32
tk-ozawa <t.aroo1512@gmail.com>
2019-09-22 02:49:09 +0900

    実データ表示 / 最下段が3件以下の場合ダミーカードを表示する

--- resources/views/report/list.blade.php
+++ resources/views/report/list.blade.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>レポートリスト画面</title>
 </head>
 <body>
@@ -15,86 +16,53 @@
 			<li class="nav-item"><a class="btn btn-danger" href="/sharereports/public/logout" role="button">ログアウト</a></li>
 		</ul>
 	</nav>
-	<div class="card-deck">
-		<div class="card">
-			<h2 class="card-header">
-				<span>レポートID</span>
-			</h2>
-			<div class="card-body">
-				<h2 class="card-title">報告者01</h2>
-			</div>
-			<ul class="list-group list-group-flush">
-				<li class="list-group-item">作業日</li>
-				<li class="list-group-item">
-					作業種類:
-					<img src="{{ asset('/img/reportcate/1.jpg') }}" alt="Card image cap" class="float-right">
-				</li>
-			</ul>
-			<div class="card-body">
-				<a href="#" class="btn btn-primary">詳細</a>
-			</div>
-		</div>
 
-		<div class="card">
-			<h2 class="card-header">
-				<span>レポートID</span>
-			</h2>
-			<div class="card-body">
-				<h2 class="card-title">報告者01</h2>
-			</div>
-			<ul class="list-group list-group-flush">
-				<li class="list-group-item">作業日</li>
-				<li class="list-group-item">
-					作業種類:
-					<img src="{{ asset('/img/reportcate/1.jpg') }}" alt="Card image cap" class="float-right">
-				</li>
-			</ul>
-			<div class="card-body">
-				<a href="#" class="btn btn-primary">詳細</a>
-			</div>
-		</div>
+	@if (session("flashMsg"))
+	<section id="flashMsg">
+		<p>{{ session("flashMsg") }}</p>
+	</section>
+	@endif
 
-		<div class="card">
-			<h2 class="card-header">
-				<span>レポートID</span>
-			</h2>
-			<div class="card-body">
-				<h2 class="card-title">報告者01</h2>
+	<?php $cnt = 0; ?>
+	@foreach ($reportList as $report)
+		@if ($cnt > 2)
+			<?php $cnt = 0; ?>
+		@endif
+		@if ($cnt === 0)
+		<div class="card-deck">
+		@endif
+			<div class="card">
+				<h2 class="card-header">
+					<span>ID:{{ $report->getId() }}</span>
+				</h2>
+				<div class="card-body">
+					<h2 class="card-title">報告者ID:{{ $report->getUserId() }}</h2>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">作業日:{{ $report->getRpDate() }}</li>
+					<li class="list-group-item">
+						作業種類:
+						<img src="../img/reportcate/1.jpg">
+						{{-- <img src="{{ asset('/img/reportcate/.jpg') }}" alt="Card image cap" class="float-right"> --}}
+					</li>
+				</ul>
+				<div class="card-body">
+					<a href="#" class="btn btn-primary">詳細</a>
+				</div>
 			</div>
-			<ul class="list-group list-group-flush">
-				<li class="list-group-item">作業日</li>
-				<li class="list-group-item">
-					作業種類:
-					<img src="{{ asset('/img/reportcate/1.jpg') }}" alt="Card image cap" class="float-right">
-				</li>
-			</ul>
-			<div class="card-body">
-				<a href="#" class="btn btn-primary">詳細</a>
+			<?php $cnt++; ?>
+			@if ($cnt % 3 === 0)
 			</div>
-		</div>
-	</div>
-	<div class="card-deck">
-		<div class="card">
-			<h2 class="card-header">
-				<span>レポートID</span>
-			</h2>
-			<div class="card-body">
-				<h2 class="card-title">報告者01</h2>
-			</div>
-			<ul class="list-group list-group-flush">
-				<li class="list-group-item">作業日</li>
-				<li class="list-group-item">
-					作業種類:
-					<img src="{{ asset('/img/reportcate/1.jpg') }}" alt="Card image cap" class="float-right">
-				</li>
-			</ul>
-			<div class="card-body">
-				<a href="#" class="btn btn-primary">詳細</a>
-			</div>
-		</div>
+			@endif
+		@endforeach
+	@if ($cnt < 2)
 		<div class="card"></div>
+	@endif
+	@if ($cnt < 3)
 		<div class="card"></div>
-	</div>
+		</div>
+	@endif
+
 
 	<script src="{{ asset('js/app.js') }}"></script>
 </body>
