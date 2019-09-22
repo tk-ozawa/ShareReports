a776c7eedf154ea66f5df2010391b6de580e7a8e
tk-ozawa <t.aroo1512@gmail.com>
2019-09-22 14:51:39 +0900

    showListのURI変更 / detail追加

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get("/logout", "LoginController@logout");	// ログアウト処理
 /**
  * レポート管理
  */
-Route::get("/reports/showList", "ReportController@showList");	// レポート一覧表示処理
+Route::get("/reports/list", "ReportController@showList");	// レポート一覧表示処理
 Route::get("/reports/goAdd", "ReportController@goAdd");	// レポート登録画面表示処理
 Route::post("/reports/add", "ReportController@add");	// レポート登録処理
-Route::get("/reports/detail", "ReportController@showDetail");	// レポート詳細画面表示処理
+Route::get("/reports/detail/{rpId}", "ReportController@showDetail");	// レポート詳細画面表示処理
