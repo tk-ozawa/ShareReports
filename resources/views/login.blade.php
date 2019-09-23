96fbc61a9fc9f871c6b40ad5e0fd5f1e37d095b7
tk-ozawa <t.aroo1512@gmail.com>
2019-09-24 00:21:54 +0900

    containerで囲った

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,34 +8,36 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/css/tempusdominus-bootstrap-4.min.css" />
 	<script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>ログイン</title>
+	<title>レポート管理システム</title>
 </head>
 <body>
-	<h1>ログイン</h1>
+	<div class="container">
+		<h1>ログイン</h1>
 
-	@isset($validationMsgs)
-	<section id="errorMsg">
-		<p>以下のメッセージをご確認ください。</p>
-		<ul>
-			@foreach ($validationMsgs as $msg)
-			<li>{{$msg}}</li>
-			@endforeach
-		</ul>
-	</section>
-	@endisset
+		@isset($validationMsgs)
+		<section id="errorMsg">
+			<p>以下のメッセージをご確認ください。</p>
+			<ul>
+				@foreach ($validationMsgs as $msg)
+				<li>{{$msg}}</li>
+				@endforeach
+			</ul>
+		</section>
+		@endisset
 
-	<form action="/sharereports/public/login" method="post">
-		@csrf
-		<div class="form-group">
-			<label for="loginUsMail">ID</label>
-			<input type="text" id="loginUsMail" class="form-control" name="loginUsMail" value="architshin@websarva.com" required>
-		</div>
-		<div class="form-group">
-			<label for="loginUsPasswd">パスワード</label>
-			<input id="loginUsPasswd" class="form-control" type="password" name="loginUsPasswd" value="hogehoge" required>
-		</div>
-		<button type="submit" class="form-control">ログイン</button>
-	</form>
+		<form action="/sharereports/public/login" method="post">
+			@csrf
+			<div class="form-group">
+				<label for="loginUsMail">ID</label>
+				<input type="text" id="loginUsMail" class="form-control" name="loginUsMail" value="architshin@websarva.com" required>
+			</div>
+			<div class="form-group">
+				<label for="loginUsPasswd">パスワード</label>
+				<input id="loginUsPasswd" class="form-control" type="password" name="loginUsPasswd" value="hogehoge" required>
+			</div>
+			<button type="submit" class="form-control">ログイン</button>
+		</form>
+	</div>
 
 	<script src="{{ asset('js/app.js') }}"></script>
 </body>
