b525f978edcc427f2dcef75bb3fadbf1a71ae572
tk-ozawa <t.aroo1512@gmail.com>
2019-09-27 10:26:45 +0900

    バリデーションエラーをアラート表示する

--- resources/views/report/edit.blade.php
+++ resources/views/report/edit.blade.php
@@ -65,10 +65,10 @@
 
 	@isset($validationMsgs)
 	<section id="errorMsg">
-		<p>以下のメッセージをご確認ください。</p>
+		<p><strong>以下のメッセージをご確認ください。</strong></p>
 		<ul>
 			@foreach ($validationMsgs as $msg)
-				<li>{{$msg}}</li>
+				<li><div class="alert alert-danger" role="alert">{{$msg}}</div></li>
 			@endforeach
 		</ul>
 	</section>
