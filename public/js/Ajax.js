587ed62b269f9452b17f9760ca23fa71069cfb9a
tk-ozawa <t.aroo1512@gmail.com>
2019-09-30 12:33:44 +0900

    レポート登録済みの作業種類が0個, 1個, 2個以上の時のプルダウンメニューに表示する内容を変更

--- public/js/Ajax.js
+++ public/js/Ajax.js
@@ -8,21 +8,29 @@ $(function () {
 				'usId': $('[id="selectUsId"]').val()
 			},
 			success: function (data) {
+				console.log("サーバー通信開始")
 				var dataArray = data.rcList
-				console.log(data.rcList)
 				$("#selectRcId").empty();
-				$("#selectRcId").append($("<option>").val("all").text("全作業種類"));
-				$.each(dataArray, function(i) {
-					$("#selectRcId").append($("<option>").val(dataArray[i].id).text(dataArray[i].id + ':' +dataArray[i].rc_name));
-				})
+				if (dataArray == null) {
+					$("#selectRcId").append($("<option>").val("all").text("作業種類無し…"));
+				}
+				else {
+					if (Object.keys(dataArray).length > 1) {
+						$("#selectRcId").append($("<option>").val("all").text("全作業種類"));
+						$.each(dataArray, function(i) {
+							$("#selectRcId").append($("<option>").val(dataArray[i].id).text(dataArray[i].id + ':' +dataArray[i].rc_name));
+						})
+					}
+					else {
+						$("#selectRcId").append($("<option>").val(dataArray[0].id).text(dataArray[0].id + ':' +dataArray[0].rc_name));
+					}
+				}
 			},
 			error: function (xhr) {
 				console.log("サーバー通信エラー")
-				alert("通信エラー")
 			},
 			complete: function (xhr) {
 				console.log("サーバー通信終了")
-				// alert("通信終了")
 			},
 		})
 	})
@@ -36,22 +44,26 @@ $(function () {
 				'usId': $('[id="selectUsId"]').val()
 			},
 			success: function (data) {
+				console.log("サーバー通信開始")
 				var dataArray = data.rcList
-				console.log(data.rcList)
 				$("#selectRcId").empty();
 				if (dataArray == null) {
 					$("#selectRcId").append($("<option>").val("all").text("作業種類無し…"));
 				}
 				else {
-					$("#selectRcId").append($("<option>").val("all").text("全作業種類"));
-					$.each(dataArray, function(i) {
-						$("#selectRcId").append($("<option>").val(dataArray[i].id).text(dataArray[i].id + ':' +dataArray[i].rc_name));
-					})
+					if (Object.keys(dataArray).length > 1) {
+						$("#selectRcId").append($("<option>").val("all").text("全作業種類"));
+						$.each(dataArray, function(i) {
+							$("#selectRcId").append($("<option>").val(dataArray[i].id).text(dataArray[i].id + ':' +dataArray[i].rc_name));
+						})
+					}
+					else {
+						$("#selectRcId").append($("<option>").val(dataArray[0].id).text(dataArray[0].id + ':' +dataArray[0].rc_name));
+					}
 				}
 			},
 			error: function (xhr) {
 				console.log("サーバー通信エラー")
-				alert("通信エラー")
 			},
 			complete: function (xhr) {
 				console.log("サーバー通信終了")
